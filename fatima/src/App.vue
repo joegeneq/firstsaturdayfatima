6ce07528d350e4270edc94c18e7e2dc69bdef94b
CodyMcCodeFace3 <alfquesada11@gmail.com>
2025-03-29 22:26:39 +0800

    New Example

--- fatima/src/App.vue
+++ fatima/src/App.vue
@@ -1,7 +1,15 @@
-
 <template>
-  <h1>Hello World!</h1>
+  <h1>{{ message }}</h1>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is some text'
+    };
+  }
+};
+</script>
+
 <style></style>
