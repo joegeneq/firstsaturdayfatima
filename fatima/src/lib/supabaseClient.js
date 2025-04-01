8be060587e39e5d7450a7eb33a1204fd8976fd22
CodyMcCodeFace3 <alfquesada11@gmail.com>
2025-04-01 08:25:18 +0000

    supabase sample

---
+++ fatima/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://xqjxjfubcqxhrcvclmqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxanhqZnViY3F4aHJjdmNsbXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0OTUzMTUsImV4cCI6MjA1OTA3MTMxNX0.-Zyw8-ONBfDGkSFfzHGSTDE1qgFVFhzejApQTUrmjK0')
