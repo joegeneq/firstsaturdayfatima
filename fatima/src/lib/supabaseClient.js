7bd1fe412980f6283a7df5650d4a6f51c9c3b7b1
joegeneq <joegeneq@users.noreply.github.com>
2025-04-02 01:12:22 +0800

    changed supabase db path

--- fatima/src/lib/supabaseClient.js
+++ fatima/src/lib/supabaseClient.js
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-  export const supabase = createClient('https://xqjxjfubcqxhrcvclmqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxanhqZnViY3F4aHJjdmNsbXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0OTUzMTUsImV4cCI6MjA1OTA3MTMxNX0.-Zyw8-ONBfDGkSFfzHGSTDE1qgFVFhzejApQTUrmjK0')
+//  export const supabase = createClient('https://xqjxjfubcqxhrcvclmqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxanhqZnViY3F4aHJjdmNsbXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0OTUzMTUsImV4cCI6MjA1OTA3MTMxNX0.-Zyw8-ONBfDGkSFfzHGSTDE1qgFVFhzejApQTUrmjK0')
+
+  export const supabase = createClient('https://eczpojfsplyajgxickzd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjenBvamZzcGx5YWpneGlja3pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MjYzNTYsImV4cCI6MjA1OTEwMjM1Nn0.vbHeKzKuux21hRMewoIxT3KcSrzqTJGCzhO9X4vpj_o')
+
