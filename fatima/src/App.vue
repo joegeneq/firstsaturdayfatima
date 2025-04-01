c70010309780d34ea8812a024cf8e86b55e4a1f0
CodyMcCodeFace3 <alfquesada11@gmail.com>
2025-04-01 08:28:33 +0000

    supabase fix 2

--- fatima/src/App.vue
+++ fatima/src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  const instruments = ref([])
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  async function getInstruments() {
+    const { data } = await supabase.from('instruments').select()
+    instruments.value = data
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  onMounted(() => {
+    getInstruments()
+  })
+  </script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  <template>
+    <ul>
+      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+    </ul>
+  </template>
