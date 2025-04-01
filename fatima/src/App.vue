a05b416f9917beb8a562d3d3c51d9fd972976e16
CodyMcCodeFace3 <alfquesada11@gmail.com>
2025-04-01 15:02:59 +0800

    Props example

--- fatima/src/App.vue
+++ fatima/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <new-component-1/>
+  <food-item food-name="Apples"/>
+  <food-item food-name="Pizza"/>
+  <food-item food-name="Rice"/>
 </template>
 
 <script></script>
@@ -10,8 +11,9 @@
   #app > div {
     border: dashed black 1px;
     display: inline-block;
+    width: 120px;
     margin: 10px;
     padding: 10px;
     background-color: lightgreen;
   }
-</style>
+</style>    
--- fatima/src/components/FoodItem.vue
+++ fatima/src/components/FoodItem.vue
@@ -1,19 +1,15 @@
 <template>
-    <div>
-      <h2>{{ name }}</h2>
-      <p>{{ message }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        name: 'Apples',
-        message: 'I like apples'
-      }
-    }
-  };
-  </script>
-  
-  <style></style>
+  <div>
+      <h2>{{ foodName }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+      'foodName'
+  ]
+};
+</script>
+
+<style></style>  
