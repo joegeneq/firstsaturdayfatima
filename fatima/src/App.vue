3bcdf63c7d3eda95803f4ba38cab637e7cf34284
joegeneq <joegeneq@users.noreply.github.com>
2025-04-02 03:16:55 +0800

    update3

--- fatima/src/App.vue
+++ fatima/src/App.vue
@@ -1,7 +1,11 @@
 <template>
+  <h1>Family</h1>
+  <router-link to="/parish-form">Add Family</router-link>
 
+  <h1>Family Members</h1>
+  <router-link to="/family-members">Add Family Member</router-link>
 
-  <router-view />
+  
 </template>
 
 <script></script>
