085da9066d7b7ea5d1b69b1734a083bbc0adf36e
joegeneq <joegeneq@users.noreply.github.com>
2025-08-26 02:32:52 +0800

    deleted Familr Member

--- fatima/src/App.vue
+++ fatima/src/App.vue
@@ -2,8 +2,7 @@
   <h1>Family</h1>
   <router-link to="/parish-form">Add Family</router-link>
 /*
-  <h1>Family Members</h1>
-  <router-link to="/family-members">Add Family Member</router-link>
+
 */
   <router-view />
 </template>
